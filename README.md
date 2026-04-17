586a8b9056f0af0a07da21969ece524ac59860bd
scubamaster96 <mathydagonlaz57@gmail.com>
2026-04-17 11:50:11 +0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Localization-Fixes
-U37.1 Fixes
+Fixes various problems with the Localization in old PAYDAY 2. Should (hopefully) work on every single version, starting from release.
+
+Notable:
+Expert Driver asset "Expert Driver ." -> "Expert Driver."
+
+
+Kick message"have been kicked." -> "has been kicked."
+
+
+Watchdogs driver death strings (ERROR: dr1_a67_any_0#)
--- localizationmanager.lua
+++ localizationmanager.lua
@@ -1,12 +1,12 @@
 Hooks:Add("LocalizationManagerPostInit", "locfixes", function(loc)
 	LocalizationManager:add_localized_strings({
-		menu_chat_peer_kicked = "$name; has been kicked.", -- have been kicked
-		menu_asset_safe_escape = "Expert Driver", -- Expert Driver .
+		menu_chat_peer_kicked = "$name; has been kicked.",
+		menu_asset_safe_escape = "Expert Driver",
 		menu_inspire_desc = "BASIC: ##$basic;##\nYou revive crew members ##50%## faster. Shouting at your teammates will increase their movement speed by ##20%## for ##10## seconds.\n\nACE: ##$pro;##\nThere is a ##75%## chance that you can revive crew members at a distance by shouting at them.", -- is it actually 75%?
-		dr1_a67_any_01 = " ", -- watchdogs day 1 car death line
-		dr1_a67_any_02 = " ", -- same as last
-		dr1_a67_any_03 = " ", -- same as last
-		dr1_a67_any_04 = " ", -- same as last
+		dr1_a67_any_01 = " ",
+		dr1_a67_any_02 = " ",
+		dr1_a67_any_03 = " ",
+		dr1_a67_any_04 = " ",
 		com_hm2_05_04 = "This is the funniest thing I watch since execution video. Now, that funny.", -- is (the) funniest
 		com_hm2_04_01 = "That's the wrong direction! Or is it? Go East. Always East.", -- thats
 		com_hm2_07_02 = "Okay, okay, we talk. You want money? Girls? Cars? Guns?", -- 2 OKs
